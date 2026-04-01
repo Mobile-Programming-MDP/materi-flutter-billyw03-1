545b8ebadd4509d254029762a47cf26a4438ed56
billyw03 <yohanesbillywicaksono_2327240155@mhs.mdp.ac.id>
2026-04-01 18:39:45 +0700

    update

--- daftar_belanja/lib/main.dart
+++ daftar_belanja/lib/main.dart
@@ -16,6 +16,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Daftar Belanja',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
       home: const ShoppingListScreen(),
     );
   }

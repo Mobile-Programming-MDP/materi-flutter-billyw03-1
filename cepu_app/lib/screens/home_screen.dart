e66c73d9ee8ee9793541906e602e7fd0a26ab8c0
billyw03 <yohanesbillywicaksono_2327240155@mhs.mdp.ac.id>
2026-04-02 12:59:54 +0700

    update

--- cepu_app/lib/screens/home_screen.dart
+++ cepu_app/lib/screens/home_screen.dart
@@ -1,13 +1,73 @@
+import 'package:cepu_app/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> _signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => const SignInScreen()),
+      (route) => false,
+    );
+  }
+
+  String? _idToken = "";
+  String? _uid = "";
+  String? _email = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getFirebaseAuthUser();
+  }
+
+  Future<void> getFirebaseAuthUser() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      _uid = user.uid;
+      _email = user.email;
+      await user
+          .getIdToken(true)
+          .then(
+            (v) => {
+              setState(() {
+                _idToken = v;
+              }),
+            },
+          );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home Screen")),
-      body: const Center(child: Text("You Have Been Signed In!")),
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        backgroundColor: Colors.blueAccent,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _signOut(context),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Your have been signed in with ID Token: $_idToken"),
+            Text("current User: $_uid"),
+            Text("Current Email: $_email"),
+          ],
+        ),
+      ),
     );
   }
 }
